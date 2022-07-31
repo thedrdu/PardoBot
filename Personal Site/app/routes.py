28cb368f6d40bcf6509159fa7f6e2c79962e2c8a
thedrdu <thedrdu@gmail.com>
2022-07-30 21:39:40 -0400

    website progress

--- Personal Site/app/routes.py
+++ Personal Site/app/routes.py
@@ -1,6 +1,7 @@
+from flask import render_template, redirect, url_for, request #rendering converts templates into complete html pages
 from app._init_ import app
 
 @app.route('/') #decorator
 @app.route('/index') #decorator
 def index():
-    return "Hello, World!"
+    return render_template('index.html', title='Home')
