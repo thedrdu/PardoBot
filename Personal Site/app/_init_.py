b0332570d651cc4bdab0f77f58f4f65d53f9f7f6
thedrdu <thedrdu@gmail.com>
2022-07-31 15:03:26 -0400

    website update

--- Personal Site/app/_init_.py
+++ Personal Site/app/_init_.py
@@ -1,8 +1,10 @@
 from flask import Flask
+# from waitress import serve
 
 app = Flask(__name__) 
 #creates the application object as an instance of class Flask imported from the flask package
 #_name_ is predefined as the name of the module in which it is used
+# serve(app, host='0.0.0.0', port=80)
 
 from app import routes
 #routes needs to import app so we put it below
