c689b3e3ede815d122e0176effbff02855e98ccf
thedrdu <thedrdu@gmail.com>
2022-07-31 17:56:18 -0400

    update init

--- Personal_Site/app/_init_.py
+++ Personal_Site/app/_init_.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 #_name_ is predefined as the name of the module in which it is used
 # serve(app, host='0.0.0.0', port=80)
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=80)
+    app.run(host='18.188.129.222',port=80)
 
 from app import routes
 #routes needs to import app so we put it below
